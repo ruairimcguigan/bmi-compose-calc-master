91678fdb9b609c6cdeb93adca48bdfda99b34028
Ruairi McGuigan <rmcguigan@aquidigital.co.uk>
2021-02-03 07:21:32 +0000

    normal weight widget

--- app/src/main/java/com/bmi/compose/ui/screens/ResultScreen.kt
+++ app/src/main/java/com/bmi/compose/ui/screens/ResultScreen.kt
@@ -1,5 +1,6 @@
 package com.bmi.compose.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -12,7 +13,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.annotatedString
@@ -27,6 +32,7 @@ import com.bmi.compose.navigateTo
 import com.bmi.compose.theme.AppTheme
 import com.bmi.compose.theme.NormalWeightColor
 import com.bmi.compose.theme.accentColor
+import com.bmi.compose.theme.foregroundColor
 import com.bmi.compose.ui.widgets.RoundIconButton
 import com.bmi.compose.ui.widgets.RoundedButton
 import com.bmi.compose.util.BmiCalculator
@@ -44,8 +50,13 @@ fun ResultScreen(
         topBar = {
             Toolbar(
                 title = stringResource(R.string.bmi_results),
-                navigationIcon = { RoundIconButton(vectorAsset = Icons.Outlined.ArrowBack, onClick = { navigateTo(Screen.Home) }) },
-                actions = { RoundIconButton(vectorAsset = Icons.Outlined.Person, onClick = { }) }) },
+                navigationIcon = {
+                    RoundIconButton(
+                        vectorAsset = Icons.Outlined.ArrowBack,
+                        onClick = { navigateTo(Screen.Home) })
+                },
+                actions = { RoundIconButton(vectorAsset = Icons.Outlined.Person, onClick = { }) })
+        },
         bodyContent = { Content(bmi) }
     )
 }
@@ -60,6 +71,35 @@ private fun Content(viewState: ResultViewState) = when (viewState) {
         ) {
 
             Card(
+
+                elevation = 0.dp,
+                backgroundColor = NormalWeightColor,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                BasicText(
+
+                    text = annotatedString {
+                        append(stringResource(R.string.your_bmi_prepend))
+                        withStyle(
+                            style = SpanStyle(
+                                color = foregroundColor,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = TextUnit.Sp(32)
+                            )
+                        ) {
+                            append(viewState.result.bmiString)
+                        }
+                    },
+
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.Black.copy(alpha = 0.6f),
+                        fontSize = TextUnit.Sp(32)
+                    )
+                )
+            }
+
+            Card(
                 shape = CircleShape,
                 elevation = 4.dp,
                 backgroundColor = MaterialTheme.colors.background,
@@ -67,45 +107,66 @@ private fun Content(viewState: ResultViewState) = when (viewState) {
             ) {
                 Card(
                     shape = CircleShape,
-                    modifier = Modifier.padding(32.dp),
+                    modifier = Modifier.padding(18.dp),
                     backgroundColor = accentColor
                 ) {
                     Box(
                         modifier = Modifier
-                            .background(MaterialTheme.colors.background, CircleShape)
+                            .background(NormalWeightColor, CircleShape)
                             .preferredSize(112.dp)
                             .padding(8.dp),
                         alignment = Alignment.Center
                     ) {
-                        BasicText(
-                            text = viewState.result.bmiString,
-                            style = TextStyle(
-                                fontWeight = FontWeight.SemiBold,
-                                color = Color.Black.copy(alpha = 0.8f),
-                                fontSize = TextUnit.Sp(32)
-                            )
+                        Image(
+                            imageResource(id = R.drawable.correct_weight),
+                            contentScale = ContentScale.Inside,
+                            modifier = Modifier.fillMaxSize().padding(8.dp)
                         )
                     }
                 }
             }
-            // "You have 21.5 body weight"
-            Text(
-                style = textStyle.copy(
-                    fontSize = TextUnit.Sp(18)
+
+            BasicText(
+                text = stringResource(R.string.normal_result_weight),
+                style = TextStyle(
+                    fontWeight = FontWeight.SemiBold,
+                    color = foregroundColor,
+                    fontSize = TextUnit.Sp(28),
                 ),
-                text = annotatedString {
-                    append("You have ")
-                    withStyle(
-                        style = SpanStyle(color = accentColor, fontWeight = FontWeight.Bold)
-                    ) {
-                        append(viewState.result.bmiString)
-                    }
-                    append(" body weight!")
-                },
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
+
+            Column(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+
+                BasicText(
+                    text = stringResource(R.string.normal_result_maintain_healthy_message),
+                    style = TextStyle(
+                        fontWeight = FontWeight.Medium,
+                        color = foregroundColor,
+                        fontSize = TextUnit.Sp(18),
+                    ),
+                    modifier = Modifier.align(Alignment.CenterHorizontally).padding(16.dp)
+                )
+
+                RowAdvice(
+                    resId = R.drawable.stay_active,
+                    advice = stringResource(R.string.advice_stay_active)
+                )
+                RowAdvice(
+                    resId = R.drawable.no_junk_food,
+                    advice = stringResource(R.string.advice_healthy_diet)
+                )
+                RowAdvice(
+                    resId = R.drawable.sleep_well,
+                    advice = stringResource(R.string.advice_sufficient_sleep)
+                )
+            }
+
+            // details button
             RoundedButton(
-                text = "Details",
+                text = stringResource(R.string.result_more_info),
                 onClick = { navigateTo(Screen.Info(viewState.result)) },
                 modifier = Modifier.align(Alignment.CenterHorizontally).preferredWidth(120.dp),
                 backGroundColor = MaterialTheme.colors.background,
@@ -113,9 +174,27 @@ private fun Content(viewState: ResultViewState) = when (viewState) {
             )
         }
     }
-    is Underweight -> { }
-    is Overweight -> { }
-    is ResultViewState.Preview -> { }
+    is Underweight -> {
+    }
+    is Overweight -> {
+    }
+    is ResultViewState.Preview -> {
+    }
+}
+
+@Composable
+fun RowAdvice(resId: Int, advice: String) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Image(
+            vectorResource(id = resId),
+            contentScale = ContentScale.Fit
+        )
+        Text(advice)
+
+    }
 }
 
 @Preview
